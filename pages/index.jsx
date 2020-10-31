b9686a97465e3ee9b79a951156bf2e4a3bea2fd8
MarkoCvrljak <marko.cvrljak1@gmail.com>
2020-10-31 11:13:42 +0100

    editor config, eslint, first page added

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function HomePage() {
+  return (
+    <div>
+      Hello World from CMG!
+    </div>
+  );
+}
+
+HomePage.propTypes = {};
+
+export default HomePage;
